b01b80f8ddad8f480d7d565c988d395ec2206476
Leidy Zambrano <zambranoleidy925@gmail.com>
2025-11-10 15:33:06 -0500

    Inicio

--- ProyFront/src/app/app.ts
+++ ProyFront/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './nav-bar/nav-bar';
 import { Footer } from './footer/footer';
+import { Inicio } from './inicio/inicio';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBar,Footer],
+  imports: [RouterOutlet,NavBar,Inicio,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

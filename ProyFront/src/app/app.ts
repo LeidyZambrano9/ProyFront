5c79f4899298c3ce3ed3b1e1261476bf803430f9
Dani2343 <dfrodriguezgarcia@ucompensar.edu.co>
2025-11-15 11:02:32 -0500

    Revert "Categorias4Duke200"
    This reverts commit 2dddbddd91edd7ecca6c7946868cde6a1843e6ab.

--- ProyFront/src/app/app.ts
+++ ProyFront/src/app/app.ts
@@ -4,12 +4,11 @@ import { NavBar } from './nav-bar/nav-bar';
 import { Footer } from './footer/footer';
 import { Inicio } from './inicio/inicio';
 import { Categorias3 } from './categorias3/categorias3';
-import { Categorias4duke200 } from './categorias4duke200/categorias4duke200';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBar, Footer, Categorias4duke200],
+  imports: [RouterOutlet, NavBar, Footer, Categorias3],
 
   templateUrl: './app.html',
   styleUrl: './app.css'

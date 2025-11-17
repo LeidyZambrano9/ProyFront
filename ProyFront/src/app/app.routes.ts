3cd3e502a8919c26108f74afa2a0adbfb5b3501f
Leidy Zambrano <zambranoleidy925@gmail.com>
2025-11-16 22:36:51 -0500

    Componente  de contacto

--- ProyFront/src/app/app.routes.ts
+++ ProyFront/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { CategoriasComponent } from './categorias/categorias';
+import { Contacto } from './contacto/contacto';
 
 
 export const routes: Routes = [
   { path: '', component: Inicio },
   { path: 'categorias', component: CategoriasComponent},
+  { path: 'contacto', component: Contacto},
   
 ];
 
--- ProyFront/src/app/app.ts
+++ ProyFront/src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './nav-bar/nav-bar';
 import { Footer } from './footer/footer';
-import { Inicio } from './inicio/inicio';
+import { Contacto } from './contacto/contacto';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBar, Footer],
+  imports: [RouterOutlet, NavBar, Footer, Contacto ],
 
   templateUrl: './app.html',
   styleUrl: './app.css'
